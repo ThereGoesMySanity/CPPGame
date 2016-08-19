94b9aa9a98f33763f5757b2f0241794165479eca
ThereGoesMySanity <willmlb@sbcglobal.net>
2016-08-18 20:23:10 -0400

    finally graphics working

--- Game/src/headers/Graphics.h
+++ Game/src/headers/Graphics.h
@@ -1,6 +1,10 @@
 #ifndef GRAPHICS_H
 #define GRAPHICS_H
 
+#include <SDL2/SDL_image.h>
+#include <string>
+#include <map>
+
 struct SDL_Window;
 struct SDL_Renderer;
 
@@ -9,9 +13,15 @@ public:
 	Graphics();
 	~Graphics();
 
+	SDL_Surface* loadImage(const std::string &filePath);
+	void blitSurface(SDL_Texture* img, SDL_Rect* src, SDL_Rect* dest);
 	void flip();
+	void clear();
+	SDL_Renderer* getRenderer() const;
 private:
 	SDL_Window* _window;
 	SDL_Renderer* _renderer;
+
+	std::map<std::string, SDL_Surface*> _sprites;
 };
 #endif
--- Game/src/src/Graphics.cpp
+++ Game/src/src/Graphics.cpp
@@ -8,7 +8,7 @@
 
 #include <Graphics.h>
 #include <SDL2/SDL.h>
-
+#include <SDL2/SDL_image.h>
 Graphics::Graphics(){
 	SDL_CreateWindowAndRenderer(800,600, 0, &this->_window, &this->_renderer);
 	SDL_SetWindowTitle(this->_window, "Game thing");
@@ -16,4 +16,24 @@ Graphics::Graphics(){
 
 Graphics::~Graphics(){
 	SDL_DestroyWindow(this->_window);
+}
+
+SDL_Surface* Graphics::loadImage(const std::string &filePath){
+	if(_sprites.count(filePath) == 0){
+		_sprites[filePath] = IMG_Load(filePath.c_str());
+	}
+	return _sprites[filePath];
+}
+
+void Graphics::blitSurface(SDL_Texture* img, SDL_Rect* src, SDL_Rect* dest){
+	SDL_RenderCopy(_renderer, img, src, dest);
+}
+void Graphics::flip(){
+	SDL_RenderPresent(_renderer);
+}
+void Graphics::clear(){
+	SDL_RenderClear(_renderer);
+}
+SDL_Renderer* Graphics::getRenderer() const {
+	return _renderer;
 }
