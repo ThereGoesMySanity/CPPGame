5d1afcb1e5d17db955b77a09c98373f44826447f
Will K <willmlb@sbcglobal.net>
2016-08-24 19:46:44 -0400

    headers

---
+++ Game/src/headers/Globals.h
@@ -0,0 +1,18 @@
+/*
+ * Globals.h
+ *
+ *  Created on: Aug 24, 2016
+ *      Author: will
+ */
+
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+namespace globals {
+const int WIDTH = 800;
+const int HEIGHT = 600;
+}
+
+
+
+#endif /* SRC_HEADERS_GLOBALS_H_ */
